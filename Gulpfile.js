ee70f216ca6ee13f5dd8236759e03f43bbb1e9bf
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-05 12:46:34 -0400

    serving static files and view engine with pug(jade).

---
+++ Gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+
+gulp.task('styles', () => {
+  gulp
+    .src('index.scss')
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['styles']);
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const port = 3000
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.listen(port, (err) => {
+  if (err) console.log(`Server Error: ${err}`), process.exit(1);
+
+  console.log(`Platzigram listen in port: ${port}`);
+})
