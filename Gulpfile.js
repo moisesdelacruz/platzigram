037fc0670174a2d10584b77ecfa3d0cf0eba4aa1
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-05 18:02:44 -0400

    add watchify to gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ const rename = require('gulp-rename');
 const babelify = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const watchify = require('watchify');
 
 gulp.task('styles', () => {
   gulp
@@ -19,13 +20,29 @@ gulp.task('assets', () => {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('scripts', () => {
-  browserify('./src/index.js')
-    .transform(babelify)
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('public'));
-});
+function compile(watch) {
+  var bundle = watchify(browserify('./src/index.js'))
 
-gulp.task('default', ['styles', 'assets', 'scripts']);
+  function rebundle() {
+    bundle
+      .transform(babelify)
+      .bundle()
+      .pipe(source('index.js'))
+      .pipe(rename('app.js'))
+      .pipe(gulp.dest('public'));
+  }
+
+  if (watch) {
+    bundle.on('update', () => {
+      console.log('--> Bundling...');
+      rebundle();
+    })
+  }
+
+  rebundle()
+}
+
+gulp.task('build', () => { return compile() });
+gulp.task('watch', () => { return compile(true) });
+
+gulp.task('default', ['styles', 'assets', 'build']);
