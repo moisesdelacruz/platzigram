19928940b6a6374b1437f56a9ddc0f8fd0a4c36b
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-05 14:20:46 -0400

    add Tiny Express-inspired client-side router

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/signup', (req, res) => res.render('index'));
+app.get('/login', (req, res) => res.render('index'));
 
 app.listen(port, (err) => {
   if (err) console.log(`Server Error: ${err}`), process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('Hola')
+const page = require('page');
+
+const main = document.getElementById('main-container');
+
+page('/', (ctx, next) => {
+  main.innerHTML = 'Home <a href="/signup">Signup</a>';
+});
+
+page('/signup', (ctx, next) => {
+  main.innerHTML = 'Signup <a href="/">Home</a>';
+});
+
+page();
