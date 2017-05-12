2061eb0f9b11e6d2b7979b953a528cbcd2a68093
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-05-12 00:16:53 -0400

    add card and image to he timeline

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,26 @@ const title = require('title');
 page('/', (ctx, next) => {
   title('Platzigram');
   const main = document.getElementById('main-container');
-  empty(main).appendChild(template);
+
+  var pictures = [
+    {
+      user: {
+        username: 'christinagrimmie',
+        avatar: 'https://lh3.googleusercontent.com/-Pl7iWsKpcQw/AAAAAAAAAAI/AAAAAAAAAAA/AHalGhoVSE--jr86LIMqaMSMMhJfBrmR8g/s32-c-mo/photo.jpg'
+      },
+      url: 'http://materializecss.com/images/sample-1.jpg',
+      likes: 152,
+      liked: false
+    },
+    {
+      user: {
+        username: 'moisesdelacruz',
+        avatar: 'https://lh3.googleusercontent.com/-Pl7iWsKpcQw/AAAAAAAAAAI/AAAAAAAAAAA/AHalGhoVSE--jr86LIMqaMSMMhJfBrmR8g/s32-c-mo/photo.jpg'
+      },
+      url: 'office.jpg',
+      likes: 1024,
+      liked: true
+    }
+  ]
+  empty(main).appendChild(template(pictures));
 });
--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,12 +1,16 @@
 const yo = require('yo-yo');
 const layout = require('../layout');
+const picture = require('../picture-card');
 
-const template = yo`<div class="container timeline">
-  <div class="row">
-    <div class="col s12 m10 offset-m1 l6 offset-l3">
-      content
+module.exports = function (pictures) {
+  var el = yo`<div class="container timeline">
+    <div class="row">
+      <div class="col s12 m10 offset-m1 l6 offset-l3">
+        ${pictures.map((pic) => {
+          return picture(pic);
+        })}
+      </div>
     </div>
-  </div>
-</div>`
-
-module.exports = layout(template)
+  </div>`
+  return layout(el)
+}
