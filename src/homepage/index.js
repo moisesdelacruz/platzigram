48fe1be728c0ee7ce7258fb4e16aa414f51daeb3
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2017-07-03 12:43:22 -0400

    Utilizando FormatJS para internalizar los textos.

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -14,7 +14,7 @@ page('/', (ctx, next) => {
         avatar: 'https://lh3.googleusercontent.com/-Pl7iWsKpcQw/AAAAAAAAAAI/AAAAAAAAAAA/AHalGhoVSE--jr86LIMqaMSMMhJfBrmR8g/s32-c-mo/photo.jpg'
       },
       url: 'http://materializecss.com/images/sample-1.jpg',
-      likes: 152,
+      likes: 0,
       liked: false,
       createdAt: new Date()
     },
@@ -24,9 +24,19 @@ page('/', (ctx, next) => {
         avatar: 'https://lh3.googleusercontent.com/-Pl7iWsKpcQw/AAAAAAAAAAI/AAAAAAAAAAA/AHalGhoVSE--jr86LIMqaMSMMhJfBrmR8g/s32-c-mo/photo.jpg'
       },
       url: 'http://materializecss.com/images/sample-1.jpg',
-      likes: 1024,
+      likes: 1,
       liked: true,
       createdAt: new Date().setDate(new Date().getDate() - 10)
+    },
+    {
+      user: {
+        username: 'demilovato',
+        avatar: 'https://lh3.googleusercontent.com/-Pl7iWsKpcQw/AAAAAAAAAAI/AAAAAAAAAAA/AHalGhoVSE--jr86LIMqaMSMMhJfBrmR8g/s32-c-mo/photo.jpg'
+      },
+      url: 'http://materializecss.com/images/sample-1.jpg',
+      likes: 7,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 7)
     }
   ]
   empty(main).appendChild(template(pictures));
--- src/picture-card/index.js
+++ src/picture-card/index.js
@@ -1,16 +1,5 @@
 const yo = require('yo-yo');
-window.IntlRelativeFormat = require('intl-relativeformat');
-
-if (!window.Intl) {
-    window.Intl = require('intl');
-    require('intl/locale-data/jsonp/en-US.js');
-    require('intl/locale-data/jsonp/es.js');
-}
-
-require('intl-relativeformat/dist/locale-data/en.js');
-require('intl-relativeformat/dist/locale-data/es.js');
-
-var rf = new window.IntlRelativeFormat('es');
+const translate = require('../translate');
 
 module.exports = function (pic) {
   var el;
@@ -25,11 +14,11 @@ module.exports = function (pic) {
           <img src="${picture.user.avatar}" class="avatar" />
           <span class="username">${picture.user.username}</span>
         </a>
-        <small class="right time">${rf.format(picture.createdAt)}</small>
+        <small class="right time">${translate.date.format(picture.createdAt)}</small>
         <p>
           <a href="#" onclick=${like.bind(null, true)} class="left"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
           <a href="#" onclick=${like.bind(null, false)} class="left"><i class="fa fa-heart" aria-hidden="true"></i></a>
-          <span class="left likes">${picture.likes} me gusta</span>
+          <span class="left likes">${translate.message('likes', { likes: picture.likes })}</span>
         </p>
       </div>
     </div>`
