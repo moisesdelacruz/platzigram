93eea098cb2e77c93bb6f46d956721b8b2979e8a
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2017-07-06 10:12:52 -0400

    add webcamjs

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -5,12 +5,64 @@ const title = require('title');
 const request = require('superagent');
 const axios = require('axios');
 const header = require('../header');
+const Webcam = require('webcamjs');
 
 page('/', header, loading, loadPictures, (ctx, next) => {
   title('Platzigram');
   const main = document.getElementById('main-container');
 
   empty(main).appendChild(template(ctx.pictures));
+
+  const picturePreview = document.getElementById('picture-preview')
+  const camaraInput = document.getElementById('camara-input')
+  const cancelPicture = $('#cancelPicture');
+  const shootButton = $('#shoot');
+  const uploadButton = $('#uploadButton');
+
+  function reset() {
+    picturePreview.classList.add('hide');
+    camaraInput.classList.remove('hide');
+    cancelPicture.addClass('hide');
+    shootButton.removeClass('hide');
+    uploadButton.addClass('hide');
+  }
+
+  cancelPicture.click(reset);
+
+  $('.modal').modal({
+    ready: function () {
+      Webcam.attach('#camara-input');
+      shootButton.click(() => {
+        Webcam.snap((data_uri) => {
+          picturePreview.innerHTML = `<img src="${data_uri}"/>`;
+          picturePreview.classList.remove('hide');
+          camaraInput.classList.add('hide');
+          cancelPicture.removeClass('hide');
+          shootButton.addClass('hide');
+          uploadButton.removeClass('hide')
+          uploadButton.off('click')
+          uploadButton.click(() => {
+            const pic = {
+              url: data_uri,
+              likes: 0,
+              liked: false,
+              createdAt: +new Date(),
+              user: {
+                avatar: "https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767",
+                username: "slifszyc"
+              }
+            }
+            $('#picture-cards').prepend(picture(pic));
+            reset();
+            $('#modalCamara').modal('close');
+          });
+        });
+      })
+    },
+    complete: function () {
+      Webcam.reset();
+    }
+  });
 });
 
 function loading (ctx, next) {
