a22477e89daf22d9e954617e7dc37e1656af9786
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2017-07-06 08:30:48 -0400

    add route '/' to logo.

--- src/header/index.js
+++ src/header/index.js
@@ -7,7 +7,7 @@ var el = yo`<nav class="header">
     <div class="container">
       <div class="row">
         <div class="col s8 m6 offset-s1">
-          <a href="#" class="brand-logo platzigram">Platzigram</a>
+          <a href="/" class="brand-logo platzigram">Platzigram</a>
         </div>
         <div class="col s4 m2 push-s8 push-m4">
           <a href="#" class="btn btn-large btn-flat dropdown-button" data-activates="drop-user">
